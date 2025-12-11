9354474a12c730af12bc20817ab514c0cb8fe179
Filipov Anatolii <anatolii.filippov@icloud.com>
2025-12-11 22:24:29 +0200

    ts & css

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 
 export default defineConfig({
-  base: "/02-react-cafe/",
+  base: '/02-react-cafe/',
   plugins: [react()],
   build: {
     sourcemap: true,
